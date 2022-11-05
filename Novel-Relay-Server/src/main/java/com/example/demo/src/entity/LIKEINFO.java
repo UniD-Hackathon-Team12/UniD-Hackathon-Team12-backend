3c9d3e7ac6865dcbde2db9745d74d16205701ba0
chaehuiseon <smilechs34@gmail.com>
2022-11-05 22:12:57 +0900

    signup error fix

---
+++ Novel-Relay-Server/src/main/java/com/example/demo/src/entity/LIKEINFO.java
@@ -0,0 +1,37 @@
+package com.example.demo.src.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "LIKEINFO")
+public class LIKEINFO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long like_id;
+
+
+    @Builder.Default
+    private boolean likeinfo = true;
+
+
+    @ManyToOne(fetch = LAZY) // N:1 단방향
+    @JoinColumn(name = "novel_id")
+    private NOVEL novel;
+
+    @ManyToOne(fetch = LAZY) // N:1 단방향
+    @JoinColumn(name = "user_id")
+    private USER user;
+}
