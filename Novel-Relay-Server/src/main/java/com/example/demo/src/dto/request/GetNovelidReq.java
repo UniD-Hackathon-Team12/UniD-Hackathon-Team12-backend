5dd32b49d6dbb8bb3db1e151190930377b35c59d
sunwookim <sunkim09@g.skku.edu>
2022-11-05 21:45:35 +0900

    ADD novel files

--- Novel-Relay-Server/src/main/java/com/example/demo/src/dto/request/GetNovelidReq.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.src.dto.request;
-
-public class GetNovelidReq {
-
-}
--- Novel-Relay-Server/src/main/java/com/example/demo/src/dto/response/GetNovelIdRes.java
+++ Novel-Relay-Server/src/main/java/com/example/demo/src/dto/response/GetNovelIdRes.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class GetNovelidRes {
+public class GetNovelIdRes {
 
 
 }
