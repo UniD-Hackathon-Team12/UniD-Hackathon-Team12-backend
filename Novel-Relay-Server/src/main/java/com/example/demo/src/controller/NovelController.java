c884a7245e71141aa86970021236869a7a96b964
sunwookim <sunkim09@g.skku.edu>
2022-11-06 00:20:15 +0900

    FEAT api

--- Novel-Relay-Server/src/main/java/com/example/demo/src/controller/NovelController.java
+++ Novel-Relay-Server/src/main/java/com/example/demo/src/controller/NovelController.java
@@ -4,6 +4,7 @@ package com.example.demo.src.controller;
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.dto.response.GetNovelIdRes;
+import com.example.demo.src.entity.RELAY;
 import com.example.demo.src.service.NovelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +22,8 @@ public class NovelController {
         this.novelService = novelService;
     }
 
-//    @GetMapping("/{novel_id}")
-//    public List<>
+    @GetMapping("/{novel_id}")
+    public BaseResponse<List<RELAY>> getRelayGroup(Long novel_id)
 
 
 }
